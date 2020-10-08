a2d984a73d4fa536c50f5fbba4f3d0ea11e17591
Lauren Judge <48807725+laurenjudge@users.noreply.github.com>
2020-10-08 16:12:41 +1100

    Updated Store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,33 +6,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    url: "http://localhost:3000/posts"
   },
   mutations: {},
   actions: {
-    getPosts(context) {
-      return axios.get(context.state.url).then(res => {
-        const data = res.data;
-        return data.map(post => ({
-          ...post,
-          createdAt: new Date(post.createdAt)
-        }
-        ));
-      })
-      .catch(err => {
-        return err;
-      })
-    },
-    newPost(title, text) {
-      return axios.post("http://localhost:3000/posts", {
-        title,
-        text
-      })
-    },
-    deletePost(id) {
-      console.log(id)
-      return axios.delete(`http://localhost:3000/posts/${id}`);
-    }
   },
   modules: {}
 });
